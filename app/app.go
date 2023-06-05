f1c394cf87b9be9bd6ade793aa480ba1f397cb8d
Taciano <taciano.dev@outlook.com>
2023-06-05 13:33:30 -0300

    feat(app): criar funcionalidade de busca por IP

--- app/app.go
+++ app/app.go
@@ -1,12 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-// Gerar aplicação
+	"github.com/urfave/cli"
+)
+
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
-	app.Usage = "Busca IPs e Nomes de Servidor da internet"
+	app.Usage = "Busca IPS e Nomes de Servidor da internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Buscar IPS de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
